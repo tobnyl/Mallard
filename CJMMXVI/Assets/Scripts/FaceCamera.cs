720c9b8dfbd4284ba4cdb5701cf153beed3ed8b9
SeruK <strandsebastian2000@gmail.com>
2016-07-05 22:04:34 +0200

    * Setup test scene with some junk * Temp old man

---
+++ CJMMXVI/Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UE = UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FaceCamera : MonoBehaviour
+{
+	#region Fields
+	#endregion
+	
+	#region Properties	
+	#endregion
+
+	#region Methods
+	void OnEnable()
+	{
+	}
+	
+	void LateUpdate()
+	{
+		var camPos = Camera.main.transform.position;
+		Vector3 selfToCamPos = camPos - transform.position;
+		var lookRot = Quaternion.LookRotation(selfToCamPos.normalized);
+		transform.rotation = lookRot;
+	}
+	#endregion
+}
