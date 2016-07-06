d7aa483af0e14ac7ac1c03ef6b5f8d5e9afb7728
SeruK <strandsebastian2000@gmail.com>
2016-07-06 15:55:24 +0200

    * More upgrade GUI work (made info + list into their own classes)

---
+++ CJMMXVI/Assets/Scripts/Gui/UpgradesListGui.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UE = UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UpgradesListGui : MonoBehaviour
+{
+	#region Types
+	public delegate void UpgradeSelectedHandler(Upgrade upgrade);
+	#endregion
+
+	#region Fields
+	[SerializeField]
+	float heightPerItem = 50.0f;
+	[SerializeField]
+	UpgradeButtonGui btnPrefab;
+	[SerializeField]
+	RectTransform contentRoot;
+
+	public UpgradeSelectedHandler onUpgradeSelected;
+
+	UpgradeManager manager;
+	List<UpgradeButtonGui> upgradeButtons;
+	#endregion
+
+	#region Methods
+	public void Setup(UpgradeManager manager)
+	{
+		this.manager = manager;
+		upgradeButtons = new List<UpgradeButtonGui>();
+
+		Upgrade[] upgrades = manager.Upgrades;
+		for(int i = 0; i < upgrades.Length; ++i)
+		{
+			Upgrade upgrade = upgrades[i];
+			AddUpgradeButton(upgrade);
+		}
+	}
+
+	public void AddUpgradeButton(Upgrade upgrade)
+	{
+		var btn = Instantiate(btnPrefab);
+		btn.upgrade = upgrade;
+		btn.SetTitle(upgrade.upgradeName);
+
+		btn.button.onClick.AddListener(() =>
+		{
+			OnButtonSelected(btn);
+		});
+
+		upgradeButtons.Add(btn);
+
+		var btnTrans = (RectTransform)btn.transform;
+		btnTrans.SetParent(contentRoot, worldPositionStays: false);
+
+		Vector2 min = contentRoot.offsetMin;
+		Vector2 max = contentRoot.offsetMax;
+		float fullHeight = manager.Upgrades.Length * heightPerItem;
+		min.y = -fullHeight / 2.0f;
+		max.y = fullHeight / 2.0f;
+		contentRoot.offsetMin = min;
+		contentRoot.offsetMax = max;
+	}
+
+	void OnButtonSelected(UpgradeButtonGui button)
+	{
+		Debug.Log("Selected " + button.upgrade.upgradeName);
+		if(onUpgradeSelected != null)
+		{
+			onUpgradeSelected(button.upgrade);
+		}
+	}
+	#endregion
+}
--- CJMMXVI/Assets/Scripts/Gui/UpgradesPage.cs
+++ CJMMXVI/Assets/Scripts/Gui/UpgradesPage.cs
@@ -8,68 +8,31 @@ using System.Collections.Generic;
 public class UpgradesPage : GuiPage
 {
 	#region Fields
-	[Header("Info")]
 	[SerializeField]
-	UI.Text titleLabel;
+	UpgradeInfoGui upgradeInfo;
 	[SerializeField]
-	UI.Text descriptionLabel;
-	[SerializeField]
-	UI.Button buyButton;
-
-	[Header("Upgrades")]
-	[SerializeField]
-	float heightPerItem = 50.0f;
-	[SerializeField]
-	UpgradeButton upgradeBtnPrefab;
-	[SerializeField]
-	RectTransform upgradesRoot;
+	UpgradesListGui upgradesList;
 
 	UpgradeManager manager;
-	List<UpgradeButton> upgradeButtons;
 	#endregion
 
 	#region Methods
 	public void Setup(UpgradeManager upgradeManager)
 	{
 		manager = upgradeManager;
-		upgradeButtons = new List<UpgradeButton>();
 
-		Upgrade[] upgrades = manager.Upgrades;
-		for(int i = 0; i < upgrades.Length; ++i)
-		{
-			Upgrade upgrade = upgrades[i];
-			AddUpgradeButton(upgrade);
-		}
+		upgradesList.Setup(manager);
+		upgradesList.onUpgradeSelected += OnUpgradeSelected;
 	}
 
-	void AddUpgradeButton(Upgrade upgrade)
+	public void OnUpgradeSelected(Upgrade upgrade)
 	{
-		var btn = Instantiate(upgradeBtnPrefab);
-		btn.upgrade = upgrade;
-		btn.SetTitle(upgrade.upgradeName);
 
-		btn.button.onClick.AddListener(() =>
-		{
-			OnButtonSelected(btn);
-		});
-
-		upgradeButtons.Add(btn);
-
-		var btnTrans = (RectTransform)btn.transform;
-		btnTrans.SetParent(upgradesRoot, worldPositionStays: false);
-
-		Vector2 min = upgradesRoot.offsetMin;
-		Vector2 max = upgradesRoot.offsetMax;
-		float fullHeight = manager.Upgrades.Length * heightPerItem;
-		min.y = -fullHeight / 2.0f;
-		max.y = fullHeight / 2.0f;
-		upgradesRoot.offsetMin = min;
-		upgradesRoot.offsetMax = max;
 	}
 
-	void OnButtonSelected(UpgradeButton button)
+	void OnBuyButtonSelected()
 	{
-		Debug.Log("Selected " + button.upgrade.upgradeName);
+	
 	}
 	#endregion
 }
