914e749ecb8040ba621594e286c00c957c4b009b
SeruK <strandsebastian2000@gmail.com>
2016-07-06 11:32:56 +0200

    * Added Vector2/3 ToAngle Extensions

---
+++ CJMMXVI/Assets/Scripts/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UE = UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Extensions
+{
+	public static float ToAngle(this Vector2 v)
+	{
+		float a = Mathf.Atan2(v.x, v.y);
+		if(v.x < 0.0f) { return 360.0f - (a * Mathf.Rad2Deg * -1.0f); }
+
+		return v.x < 0.0f ?
+			a * Mathf.Rad2Deg * -1.0f :
+			a * Mathf.Rad2Deg;
+	}
+
+	public static float ToAngleXZ(this Vector3 v)
+	{
+		return new Vector2(v.x, v.z).ToAngle();
+	}
+}
